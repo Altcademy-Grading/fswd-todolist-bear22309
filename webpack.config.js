b59c15662188301b3387624c87bad61cbcdccf8a
Tony <anthonymostrowski@gmail.com>
2024-07-25 14:03:17 -0400

    Update Babel and Webpack configurations

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,12 @@ module.exports = {
   // Entry point for your application
   entry: {
     application: './app/javascript/packs/application.js',
-    index: './app/javascript/packs/index.js'
+    index: './app/javascript/packs/index.js',
   },
   output: {
     filename: '[name]-[hash].js',
     path: path.resolve(__dirname, 'public/packs'),
-    publicPath: '/packs/'
+    publicPath: '/packs/',
   },
   module: {
     rules: [
@@ -23,34 +23,28 @@ module.exports = {
             presets: ['@babel/preset-env'],
             plugins: [
               '@babel/plugin-proposal-private-methods',
-              '@babel/plugin-proposal-private-property-in-object'
-            ]
-          }
-        }
+              '@babel/plugin-proposal-private-property-in-object',
+            ],
+          },
+        },
       },
       {
         test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/,
-        use: [
-          'file-loader'
-        ]
-      }
-    ]
+        use: ['file-loader'],
+      },
+    ],
   },
   resolve: {
-    extensions: ['.js', '.scss']
+    extensions: ['.js', '.scss'],
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'public'),
     compress: true,
-    port: 3035
-  }
+    port: 3035,
+  },
 };
 
