2493f4b7f0885c8ad9fd9e4d044639af237efca8
Tony <anthonymostrowski@gmail.com>
2024-07-29 11:49:46 -0400

    Fix authentication issues and handle unprocessable content errors

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,3 @@
 const { environment } = require('@rails/webpacker')
-const webpack = require('webpack')
-
-// Add the ProvidePlugin to the environment
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
-  $: 'jquery',
-  jQuery: 'jquery',
-  Popper: ['popper.js', 'default']
-}))
 
 module.exports = environment
-
