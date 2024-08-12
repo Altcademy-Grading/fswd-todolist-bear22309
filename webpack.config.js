e1fbb2aa84e17f06a7177aa5d6a2acfceb954572
Tony <anthonymostrowski@gmail.com>
2024-08-12 10:17:55 -0400

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -15,17 +15,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: [
-              ['@babel/plugin-transform-class-properties', { loose: true }],
-              ['@babel/plugin-transform-private-methods', { loose: true }],
-              ['@babel/plugin-transform-private-property-in-object', { loose: true }],
-            ],
-          },
-        },
+        use: 'babel-loader',
       },
       {
         test: /\.scss$/,
